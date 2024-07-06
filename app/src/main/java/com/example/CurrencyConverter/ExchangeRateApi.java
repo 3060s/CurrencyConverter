1d996e6d4ac65c0b4bdd0fdb82246ba4037e2592
3060s <78622374+3060s@users.noreply.github.com>
2024-07-06 22:32:21 +0200

    Basic version

---
+++ app/src/main/java/com/example/CurrencyConverter/ExchangeRateApi.java
@@ -0,0 +1,51 @@
+package com.example.CurrencyConverter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.net.URL;
+
+
+public class ExchangeRateApi {
+
+    final String base_url = "https://v6.exchangerate-api.com/v6/";
+    final String api_key = "9bfa0afa08db0a3aa9f1029a/latest/";
+    final String currency = "USD";
+    String url = base_url + api_key + currency;
+
+    void createRequest() throws MalformedURLException, IOException {
+        URL url = new URL(this.url);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+    }
+
+    public JSONObject getConversionRates() throws IOException, JSONException {
+        URL url = new URL(this.url);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+
+        String response = stringBuilder.toString();
+        JSONObject jsonObject = new JSONObject(response);
+
+        return jsonObject.getJSONObject("conversion_rates");
+    }
+
+    public static double calculateExchangeRate(JSONObject conversionRates, String baseCurrency, String targetCurrency) throws JSONException {
+        double baseRate = conversionRates.getDouble(baseCurrency);
+        double targetRate = conversionRates.getDouble(targetCurrency);
+        return targetRate / baseRate;
+    }
+}
---
+++ app/src/main/java/com/example/CurrencyConverter/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.CurrencyConverter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.javafirstproject.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private JSONObject conversionRates;
+
+    private class FetchConversionRatesTask extends android.os.AsyncTask<Void, Void, JSONObject> {
+        private Exception exception;
+
+        protected JSONObject doInBackground(Void... voids) {
+            try {
+                ExchangeRateApi api = new ExchangeRateApi();
+                return api.getConversionRates();
+            } catch (Exception e) {
+                this.exception = e;
+                return null;
+            }
+        }
+
+        protected void onPostExecute(JSONObject conversionRates) {
+            if (this.exception != null) {
+                this.exception.printStackTrace();
+                return;
+            }
+
+            MainActivity.this.conversionRates = conversionRates;
+
+            Spinner baseCurrencySpinner = findViewById(R.id.spinner_base_currency);
+            Spinner targetCurrencySpinner = findViewById(R.id.spinner_target_currency);
+            Iterator<String> keys = conversionRates.keys();
+            List<String> currencies = new ArrayList<>();
+            while (keys.hasNext()) {
+                currencies.add(keys.next());
+            }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, currencies);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            baseCurrencySpinner.setAdapter(adapter);
+            targetCurrencySpinner.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new FetchConversionRatesTask().execute();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Spinner baseCurrencySpinner = findViewById(R.id.spinner_base_currency);
+                Spinner targetCurrencySpinner = findViewById(R.id.spinner_target_currency);
+                TextView textView = findViewById(R.id.textViewID);
+
+                String baseCurrency = baseCurrencySpinner.getSelectedItem().toString();
+                String targetCurrency = targetCurrencySpinner.getSelectedItem().toString();
+
+                try {
+                    double exchangeRate = ExchangeRateApi.calculateExchangeRate(conversionRates, baseCurrency, targetCurrency);
+                    textView.setText("Exchange Rate: " + exchangeRate);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    textView.setText("Error calculating exchange rate");
+                }
+            }
+        });
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
